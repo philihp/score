0ac5e40abedebe2c423b028c53725d56eb8a4e02
Philihp Busby <philihp@gmail.com>
2020-06-05 23:50:52 +0000

    add injestor code

--- injest.js
+++ injest.js
@@ -2,55 +2,66 @@
 // https://gist.github.com/philihp/b4bbb546a230cdbeea38463202b47898
 // however it might come in useful again, someday
 
-const csv = require("csv-parser");
-const fs = require("fs");
+const csv = require('csv-parser')
+const fs = require('fs')
+const shuffle = require('fast-shuffle').default
 
 const players = {
-  "Ken ?": "ken",
-  "Philihp ?": "phil",
-  "Philihp Busby": "phil",
-  "Aaron Dixon": "aaron",
-  "Ville Helio": "ville",
-  "Sean George": "sean",
-  "Matt Johnson": "matt",
-  "A C": "adam",
-};
+  'Ken ?': 'ken',
+  'Philihp ?': 'phil',
+  'Philihp Busby': 'phil',
+  'Aaron Dixon': 'aaron',
+  'Ville Helio': 'ville',
+  'Sean George': 'sean',
+  'Matt Johnson': 'matt',
+  'A C': 'adam',
+}
 
-fs.createReadStream(`${__dirname}/tta2.csv`)
+fs.createReadStream(`${__dirname}/tta.csv`)
   .pipe(csv())
-  .on("data", (data) => {
-    const createdAt = new Date(data.start).toISOString();
-    const updatedAt = new Date(data.end).toISOString();
-    console.log({ createdAt, updatedAt });
-    let dat = {
-      id: data.id,
+  .on('data', (data) => {
+    const createdAt = new Date(data.date).toISOString()
+    const updatedAt = createdAt
+    const dat = {
+      id: data.name,
       createdAt,
       updatedAt,
+      location: 'TTA App',
       description: data.name,
-      game: data.game === "Through the Ages" ? "tta" : "tta2",
+      game: data.game === 'Through the Ages' ? 'tta' : 'tta2',
       players: [],
       results: [],
-    };
+    }
     if (data.g1.length > 1) {
-      dat.players.push({ id: players[data.g1] || data.g1 });
-      dat.results.push({ player: players[data.g1] || data.g1, score: data.s1 });
-      if (players[data.g1] === undefined) console.log(data.g1);
+      dat.players.push({ id: players[data.g1] || data.g1 })
+      dat.results.push({
+        player: players[data.g1] || data.g1,
+        score: Number.parseInt(data.s1, 10),
+      })
     }
     if (data.g2.length > 1) {
-      dat.players.push({ id: players[data.g2] || data.g2 });
-      dat.results.push({ player: players[data.g2] || data.g2, score: data.s2 });
-      if (players[data.g2] === undefined) console.log(data.g2);
+      dat.players.push({ id: players[data.g2] || data.g2 })
+      dat.results.push({
+        player: players[data.g2] || data.g2,
+        score: Number.parseInt(data.s2, 10),
+      })
     }
     if (data.g3.length > 1) {
-      dat.players.push({ id: players[data.g3] || data.g3 });
-      dat.results.push({ player: players[data.g3] || data.g3, score: data.s3 });
-      if (players[data.g3] === undefined) console.log(data.g3);
+      dat.players.push({ id: players[data.g3] || data.g3 })
+      dat.results.push({
+        player: players[data.g3] || data.g3,
+        score: Number.parseInt(data.s3, 10),
+      })
     }
     if (data.g4.length > 1) {
-      dat.players.push({ id: players[data.g4] || data.g4 });
-      dat.results.push({ player: players[data.g4] || data.g4, score: data.s4 });
-      if (players[data.g4] === undefined) console.log(data.g4);
+      dat.players.push({ id: players[data.g4] || data.g4 })
+      dat.results.push({
+        player: players[data.g4] || data.g4,
+        score: Number.parseInt(data.s4, 10),
+      })
     }
-    let str = JSON.stringify(dat, null, 2);
-    fs.writeFileSync(`matches/${data.id}.json`, str);
-  });
+    dat.players = shuffle(dat.players)
+    dat.results.sort((a, b) => b.score - a.score)
+    const str = JSON.stringify(dat, null, 2)
+    fs.writeFileSync(`matches/tta2/${data.name}.json`, str)
+  })
