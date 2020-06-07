962d2e45e0d9f9a6bf54bfc761960a5ca3408c60
Philihp Busby <philihp@gmail.com>
2020-06-07 07:41:38 +0000

    Build site (#4)
    * build with harp
    
    * use node 13
    
    * change build name
    
    * update package-lock
    
    * add skeleton of site
    
    * tabulate and generate stubs
    
    * lint tabulate
    
    * rename tabulate to build-site
    
    * root directory different when deployed
    
    * only build on merges to master

---
+++ build-site.js
@@ -0,0 +1,93 @@
+const fs = require('fs')
+const glob = require('glob').sync
+const {
+  compose,
+  pipe,
+  map,
+  reduce,
+  filter,
+  where,
+  equals,
+  any,
+  mergeAll,
+  sortBy,
+  view,
+  lens,
+  prop,
+  zip,
+  flatten,
+  toPairs,
+  fromPairs,
+  reverse,
+} = require('ramda')
+const { rate, rating, ordinal } = require('openskill')
+
+const forPlayer = (matches) => (player) =>
+  filter((m) => any((p) => p.id === player.id, m.players), matches)
+
+const forGame = (matches) => (game) =>
+  sortBy(prop('updatedAt'), filter(where({ game: equals(game.id) }))(matches))
+
+const reconstitute = (matches) =>
+  pipe(
+    glob,
+    map(fs.readFileSync),
+    map(JSON.parse),
+    map((record) => ({
+      ...record,
+      matches: matches(record),
+    })),
+    map((record) => ({ [record.id]: record })),
+    mergeAll
+  )
+const matches = pipe(
+  glob,
+  map(fs.readFileSync),
+  map(JSON.parse)
+)('matches/**/*.json', { ignore: ['**/match.schema.json'] })
+
+const data = {
+  players: reconstitute(forPlayer(matches))('players/**/*.json'),
+  games: reconstitute(forGame(matches))('games/**/*.json'),
+}
+
+const gameRatings = map(
+  (game) =>
+    reduce((ratings, match) => {
+      const players = map((p) => p.id, match.players)
+      const oldRatings = map((p) => [ratings[p] || rating()], players)
+      const newRatings = rate(oldRatings)
+      return { ...ratings, ...fromPairs(zip(players, flatten(newRatings))) }
+    }, {})(game.matches),
+  data.games
+)
+
+const sortedRankings = pipe(
+  map((r) => ({ ...r, ordinal: ordinal(r) })),
+  toPairs,
+  compose(sortBy, view, lens, prop)('ordinal'), // i, too, hate me for writing this.
+  reverse
+)
+
+const data2 = {
+  ...data,
+  games: map(
+    (g) => ({ ...g, players: sortedRankings(gameRatings[g.id]) }),
+    data.games
+  ),
+}
+
+const createStubsForGame = (game) => {
+  fs.writeFileSync(`./src/games/${game.id}.ejs`, '')
+}
+map(createStubsForGame, data2.games)
+fs.writeFileSync('./src/games/_data.json', JSON.stringify(data2.games, null, 2))
+
+const createStubsForPlayer = (player) => {
+  fs.writeFileSync(`./src/players/${player.id}.ejs`, '')
+}
+map(createStubsForPlayer, data2.players)
+fs.writeFileSync(
+  './src/players/_data.json',
+  JSON.stringify(data2.players, null, 2)
+)
