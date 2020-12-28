90fa3039194f7628e50804e31445ddb71838c8f5
Philihp Busby <philihp@gmail.com>
2020-12-28 14:04:22 +0000

    make the build generate new type of summary json

--- reduce-rating.js
+++ reduce-rating.js
@@ -1,12 +1,33 @@
-const { sortBy, map, fromPairs, zip, flatten } = require('ramda')
+const {
+  sortBy,
+  map,
+  zip,
+  flatten,
+  over,
+  lensProp,
+  reduce,
+  compose,
+} = require('ramda')
 const { rate, rating } = require('openskill')
+
+const updateRating = (r, match) => (oldRating) => ({
+  rating: r,
+  history: [
+    ...(oldRating?.history || []),
+    [match.id, oldRating?.rating || rating()],
+  ],
+})
 
 module.exports = (ratings, match) => {
   const players = map(
     (p) => p.player,
     sortBy((p) => -p.score, match.results)
   )
-  const oldRatings = map((p) => [ratings[p] || rating()], players)
-  const newRatings = rate(oldRatings)
-  return { ...ratings, ...fromPairs(zip(players, flatten(newRatings))) }
+  const oldRatings = map((p) => [ratings[p]?.rating || rating()], players)
+  const accumulate = reduce(
+    (accumulator, [p, r]) =>
+      over(lensProp(p), updateRating(r, match), accumulator),
+    ratings
+  )
+  return compose(accumulate, zip(players), flatten, rate)(oldRatings)
 }
